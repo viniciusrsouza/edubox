2fc4ebb24d17edd2716076cb84d732ac33c338ba
caio mota <caiocrmota@gmail.com>
2020-05-10 18:27:33 -0300

    fix base models

--- edubox/base/models.py
+++ edubox/base/models.py
@@ -1,62 +1,6 @@
 from django.db import models
 from edubox.users.models import User
 
-class Assignment(models.Model):
-    title = models.CharField(max_length=50)
-    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
-class GradedAssignment(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
-    assignment = models.ForeignKey(
-        Assignment, on_delete=models.SET_NULL, blank=True, null=True)
-    grade = models.FloatField()
-
-    def __str__(self):
-        return self.student.username
-
-class PostFile(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='uploads/')
-    
-
-class Post(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)    
-    content = models.CharField(max_length=10000)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_pinned = models.BooleanField(default=False) 
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-class Choice(models.Model):
-    title = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.title
-
-
-class Question(models.Model):
-    question = models.CharField(max_length=200)
-    choices = models.ManyToManyField(Choice)
-    answer = models.ForeignKey(
-        Choice, on_delete=models.CASCADE, related_name='answer', blank=True, null=True)
-    assignment = models.ForeignKey(
-        Assignment, on_delete=models.CASCADE, related_name='questions', blank=True, null=True)
-    order = models.SmallIntegerField()
-
-    def __str__(self):
-        return self.question
-
-class Quiz(models.Model):
-    name = models.CharField(max_length=200)
-    timeopen = models.IntegerField(default=0)
-    timeclose = models.IntegerField(default=0)
-    timelimit = models.IntegerField(default=0)
-    attempts = models.IntegerField(default=1)
-    
 
 class Assignment(models.Model):
     title = models.CharField(max_length=50)
