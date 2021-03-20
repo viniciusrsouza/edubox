c5ec09487575934bcdb469165e2502e14e3a01f1
viniciusrsouza <vinicius.rsouza07@gmail.com>
2021-03-20 15:14:00 -0300

    added vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
