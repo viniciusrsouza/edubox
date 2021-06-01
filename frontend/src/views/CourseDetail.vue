7657272b67fb7448063d35518e7baf1296859272
Phnc <paulohn27@hotmail.com>
2021-05-31 21:27:31 -0300

    added navbar to course detail

--- frontend/src/views/CourseDetail.vue
+++ frontend/src/views/CourseDetail.vue
@@ -1,25 +1,30 @@
 <template>
-  <three-column-template>
-    <template v-slot:sidebar>
-      <side-bar />
-    </template>
-    <template v-slot:content>
-      <h1>Here might be a page title</h1>
-    </template>
-    <template v-slot:calendar>
-      <h1>Here might be a page title</h1>
-    </template>
-  </three-column-template>
+  <div class="home-container">
+    <Navbar />
+    <three-column-template>
+      <template v-slot:sidebar>
+        <side-bar />
+      </template>
+      <template v-slot:content>
+        <h1>Here might be a page title</h1>
+      </template>
+      <template v-slot:calendar>
+        <h1>Here might be a page title</h1>
+      </template>
+    </three-column-template>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import SideBar from "../components/CourseDetail/SideBar.vue";
 import ThreeColumnTemplate from "../components/CourseDetail/ThreeColumnTemplate.vue";
+import Navbar from '../components/Navbar.vue'
 export default Vue.extend({
   components: {
     SideBar,
     ThreeColumnTemplate,
+    Navbar
   },
 });
 </script>
