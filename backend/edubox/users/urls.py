7f2c29d440326b35b14616eec61d0f2dae0c7a3b
caio mota <caiocesarrmota@gmail.com>
2021-06-01 17:33:03 -0300

    refactored user creation

--- backend/edubox/users/urls.py
+++ backend/edubox/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from edubox.users.views import *
+from edubox.users import views
 
 app_name = "users"
 urlpatterns = [
 
-    path("create/", view=CreateAuthUserView.as_view()),
+    path("", view=views.CreateAuthUserView.as_view()),
 ]
--- backend/edubox/users/views.py
+++ backend/edubox/users/views.py
@@ -1,17 +1,14 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
-from django.views.generic import DetailView, RedirectView, UpdateView
-from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny, DjangoModelPermissions
+from rest_framework.permissions import AllowAny
 from edubox.users.serializers import CreateAuthUserSerializer
 from django.contrib.auth.models import Group
 from rest_framework.response import Response
+from edubox.users.services import services
+from rest_framework import status
 
 User = get_user_model()
-
 
 class CreateAuthUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
@@ -20,30 +17,13 @@ class CreateAuthUserView(generics.CreateAPIView):
     serializer_class = CreateAuthUserSerializer
 
     def create(self, request, *args, **kwargs):
-        grupos = Group.objects.all()
-            
-        user = self.request.user
-        email = self.request.data['email']
-
-        if(User.objects.filter(email=email).exists()):
-            return(Response({"success": False, "message": "Email " + email + " já cadastrado."}))
-
-        serializer = self.get_serializer(data=request.data)
+        serializer = CreateAuthUserSerializer(data=request.data.copy())
         serializer.is_valid(raise_exception=True)
-        auth_user = serializer.save()
-        auth_user.username = serializer.validated_data['email']
-        if request.data['password'] != request.data['confirm_password']:
-            return Response({"success": False, 'message':'Os passwords são diferentes.'}) 
-        auth_user.set_password(request.data['password'])
-        auth_user.is_active = True
-        auth_user.save()
-        #self.send_registration_link_email(auth_user.email)
-
-        #headers = self.get_success_headers(serializer.data)
-        
-        return Response({"success": True, 'message':'Cadastrado com sucesso.'})
-
-
+        result = services.UserRegister.execute(**serializer.validated_data)
+        if result['success']:
+            return Response(result, status=status.HTTP_201_CREATED)
+        else:
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
 
 
 
